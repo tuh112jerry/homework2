3e8047acd3ac05e0c260f50c632dafe7b60a0a05
moonsword5083 <anthea880702@gmail.com>
2020-10-18 19:08:41 +0800

    Initial Commit

---
+++ homework2/homework2App.swift
@@ -0,0 +1,17 @@
+//
+//  homework2App.swift
+//  homework2
+//
+//  Created by User23 on 2020/10/18.
+//
+
+import SwiftUI
+
+@main
+struct homework2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
